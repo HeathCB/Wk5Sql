a8f966778656bdd113869483362ffe0798774173
Heathcb <Heath.c.b@gmail.com>
2020-12-18 15:12:08 -0600

    UPdate

--- Week3/src/Week3assignment.java
+++ Week3/src/Week3assignment.java
@@ -10,15 +10,15 @@ public class Week3assignment {
 	int[] ages = {3, 9, 23, 64, 2, 8, 28, 93};
 	int lengthOfAges = ages.length;
 	int difference = Math.abs(ages[ lengthOfAges - lengthOfAges ] - ages[ lengthOfAges -1]);
-		System.out.println( difference );
+		System.out.println("answer for 1"+" " + difference );
 		//1b	
 		ages[7] = 104;
-		System.out.println( difference );
+		System.out.println("answer for 1b" + difference );
 		//1c
 	int sum = 0;
 	for (int x : ages) 
 				sum +=x ;
-		System.out.println(sum / ages.length);
+		System.out.println("answer for 1c" + sum / ages.length);
 		//2
 	String[] names = { "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob" };
 	int[] nameLengths = {names[0].length(), names[1].length(), names[2].length(), names[3].length(), names[4].length(),names[5].length()};
@@ -28,7 +28,7 @@ public class Week3assignment {
 		//2a
 	int h = 0;
 	do {
-		System.out.println(IntStream.of(nameLengths).sum());
+		System.out.println(" answer for 2a"+" " + IntStream.of(nameLengths).sum());
 		h++;
 	}while ( h > nameLengths.length);
 		
@@ -36,11 +36,15 @@ public class Week3assignment {
 		//2b
 	String concat = String.join(" ", names);
 	do {
-		System.out.println(concat);
+		System.out.println(" answer for 2b" + " " + concat);
 		h++;
 	}while ( h > nameLengths.length);
 	// 3
-	// names.length
+	// names.length will give you the length of the array then 
+	// you can use example[6] to go to the final field in the array 
+	// 4 
+	// to acces the first element of any array just use example[0]. [0] will
+	// will always be the first element in any array.
 	// ALL OF MY METHODS ARE IN PRACTICE.JAVA
 
 
